1d04cf71a121cf128083b95622d9f3db4cfc5b09
Gitomm <Aaron.frank123@gmail.com>
2016-04-16 15:50:07 -0700

    Added GoblinKing -needs summonGoblin() defined

---
+++ GoblinKing.java
@@ -0,0 +1,25 @@
+public class GoblinKing extends Chara
+{
+	private Attack basic = new Attack ("Weapon", 3);
+	private Attack heavy = new Attack ("Weapon", 5);
+
+	public GoblinKing(BufferedImage i, int xo, int yo, int h, int arm, int res)
+	{
+		super(i, xo, yo, h, 4, 4);
+	}
+
+	public int basic(Chara player)
+	{
+		return this.attack(player, basic);
+	}
+
+	public int heavy(Chara player)
+	{
+		return this.attack(playe, heavy);
+	}
+
+	public void summonGoblin(Map m)
+	{
+		
+	}
+}
