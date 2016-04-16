f4627cd27c3cd0aac68d9fdb9eb491b795966daf
Gitomm <Aaron.frank123@gmail.com>
2016-04-16 12:37:24 -0700

    Edited canMove()

--- Character.java
+++ Character.java
@@ -2,7 +2,7 @@ public class Character extends Entity
 {
 	public boolean canMove(Map m, int nx, int ny)
 	{
-		return (m.map[nx][ny] instanceof terrain);
+		return (m.map[nx][ny] instanceof terrain && (nx < m.map.length && nx >= 0) && (m.map[nx].length > ny && ny >= 0));
 	}
 
 	public void move(int nx, int ny)
